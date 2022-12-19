a3f85d134dff98f2604dfc2f55a014920f3e591c
Kilian Evang <noreply@texttheater.net>
2022-12-19 03:34:17 +0100

    Moved from twitter.com to botsin.space

--- README.md
+++ README.md
@@ -3,12 +3,12 @@ SPIEGELCaptions
 
 A Twitterbot parody of German magazine DER SPIEGEL’s image captions. Takes
 existing captions and recombines them randomly, making just as much sense.
-Live at [@SPIEGELCaptions](https://twitter.com/SPIEGELCaptions).
+Live at [@SPIEGELCaptions@botsin.space](https://botsin.space/@SPIEGELCaptions).
 
 The `captions.txt` currently in the repository is based on the DER SPIEGEL
-archives of issues 22/1996—52/2015 (selected for PDF availability and
+archives of issues 22/1996—52/2021 (selected for PDF availability and
 quality).
 
 To make a random tweet and post it:
 
-    $ ./post
+    $ python3 -m post
---
+++ post.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+
+import os
+import random
+
+
+from mastodon import Mastodon
+
+
+if __name__ == '__main__':
+    mastodon = Mastodon(
+            #client_id=config.client_id,
+            #client_secret=config.client_secret,
+            #access_token=config.access_token,
+        access_token = 'token.secret',
+        api_base_url = 'https://botsin.space/',
+    )
+    #mastodon.log_in(config.email, config.password)
+    bot_home = os.path.dirname(__file__)
+    parts1 = []
+    parts2 = []
+    with open(os.path.join(bot_home, 'captions.txt')) as f:
+        for line in f:
+            line = line.rstrip()
+            part1, part2 = line.split('\t')
+            parts1.append(part1)
+            parts2.append(part2)
+    part1 = random.choice(parts1)
+    part2 = random.choice(parts2)
+    text = '{}: {}'.format(part1, part2)
+    mastodon.toot(text)
